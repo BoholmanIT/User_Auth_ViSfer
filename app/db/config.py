4f5446355232229c9aa4938a023d38d1194f8d5e
BoholmanIT <boholman.it.tech.nstu@gmail.com>
2026-04-06 14:15:45 +0700

    Сделал подключение и движки Делаю сессии (Забыл сделать отдельную ветвь

---
+++ app/db/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings): 
+    HOSTDB: str
+    USERDB: str
+    PASSDB: str
+    APPNAME: str
+    PORTDB: str
+    STACKDB: str
+    
+    @property
+    def DATABASE_URL_asynccopg(self):
+        return f"{self.STACKDB}://{self.USERDB}:{self.PASSDB}@{self.HOSTDB}:{self.PORTDB}/{self.APPNAME}"
+    
+    @property
+    def DATABASE_URL_psycopg(self):
+        return f"{self.STACKDB}://{self.USERDB}:{self.PASSDB}@{self.HOSTDB}:{self.PORTDB}/{self.APPNAME}"
+    
+    model_config = SettingsConfigDict(env_file = ".env")
+    
+settings = Settings() 
---
+++ app/db/engine.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import URL, create_engine, text
+from config import settings
+import asyncio
+
+sync_engine = create_engine(
+    url=settings.DATABASE_URL_psycopg,
+    echo=True,
+    
+)
+
+async_engine = create_async_engine(
+    url=settings.DATABASE_URL_asynccopg,
+    echo=True,
+)
+
+
