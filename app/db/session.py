5ac0cd1831972924bbfdb3863026dafbcb29c3ee
BoholmanIT <boholman.it.tech.nstu@gmail.com>
2026-04-30 03:11:12 +0700

    Пересобрал структуру после краха из-за --хард ресет который выполнил из-за непонятного конфликта

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import async_sessionmaker
 
 
 
-def Factory_AsyncSession(async_engine)):
+def Factory_AsyncSession(async_engine):
     return async_sessionmaker(
         bind=async_engine,
         expire_on_commit=False,
